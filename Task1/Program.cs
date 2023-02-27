0e34da8a59e07ba344b6f7e9e3ae2ae1c19690e0
moshinetsae <moshinetsae@mosreg.ru>
2023-02-27 14:29:36 +0200

    .

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("1. Заменить последний положительный элемент массива на второй элемент массива");
+
+int[] arr = { 1, -2, 3, -4, 5 };
+
+Console.WriteLine("Array:");
+Console.WriteLine(string.Join(", ", arr));
+
+for (var i = arr.Length - 1; i >= 0; i--)
+{
+    if (arr[i] > 0)
+    {
+        arr[i] = arr[1];
+        break;
+    }
+}
+
+Console.WriteLine("Result:");
+Console.WriteLine(string.Join(", ", arr));
