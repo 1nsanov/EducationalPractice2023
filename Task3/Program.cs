0a0848918e6b5d7059e44b6ef206b7aa3765af3a
moshinetsae <moshinetsae@mosreg.ru>
2023-02-28 09:50:35 +0200

    за 27

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,24 @@
 ﻿Console.WriteLine("3. Сформировать и вывести матрицу вида:");
+Console.WriteLine();
+
+var array = new int[5,5];
+var counter = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = j >= counter ? 1 : 2;
+    }
+    counter++;
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
